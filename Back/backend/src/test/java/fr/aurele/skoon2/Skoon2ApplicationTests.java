4c6d37e1d820c414cb0a7305304324bb3be445ad
aurele3000 <aurele.hurtis@gmail.com>
2021-03-25 22:15:07 +0100

    commit de nuit

---
+++ Back/backend/src/test/java/fr/aurele/skoon2/Skoon2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.aurele.skoon2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skoon2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
